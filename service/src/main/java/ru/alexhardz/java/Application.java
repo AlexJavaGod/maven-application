b262fcef36e9ce3db7cc19de8c1ce9af2870349f
Electroman <s@mail.ru>
2022-12-17 21:52:39 +0300

    New lesson.

---
+++ service/src/main/java/ru/alexhardz/java/Application.java
@@ -0,0 +1,20 @@
+package ru.alexhardz.java;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Application {
+
+
+    public static void main(String[] args) {
+
+        log.error("yo bro!");
+
+    }
+
+
+
+
+
+}
